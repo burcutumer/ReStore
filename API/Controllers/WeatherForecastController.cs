44139a63b36e8e405cad63b3cc1334dd04082755
Burcu Tumer <burcukartal17@gmail.com>
2022-07-09 13:38:27 +0300

    End of section 2

---
+++ API/Controllers/WeatherForecastController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly BurcuServis _burcuServis;
+        public WeatherForecastController(BurcuServis servis)
+        {
+            _burcuServis = servis;
+        }
+
+        [HttpGet]
+        public int Get()
+        {
+            var x = _burcuServis.Age;
+            _burcuServis.Age = 110;
+            return x;
+        }
+    }
+}
---
+++ API/Data/Migrations/20220707102338_PictureUrlCorrected.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    public partial class PictureUrlCorrected : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "MPictureUrlyProperty",
+                table: "Products",
+                newName: "PictureUrl");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PictureUrl",
+                table: "Products",
+                newName: "MPictureUrlyProperty");
+        }
+    }
+}
